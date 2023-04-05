5b9afdc1543cfa0fe11453145226d758873fa25f
stupendousweb <topher@stupendousweb.com>
2023-04-05 12:36:41 -0700

    Think twice, code once.

--- pages/api/query.js
+++ pages/api/query.js
@@ -2,18 +2,21 @@ const { MongoClient } = require("mongodb");
 const client = new MongoClient(process.env.MONGO_DB_URI);
 
 export default async function handler(request, response) {
-  response.status(200).json("Good things come to those who wait.");
-
-  /*
-
-  await client.connect();
-
-  const result = await client
-    .db("stupendous-analytics")
-    .collection("pageviews")
-    .updateMany({}, { $rename: { created_at: "createdAt" } })
-    .finally(() => client.close());
-
-
-   */
+  // await client.connect();
+  // const collection = await client
+  //   .db("stupendous-analytics")
+  //   .collection("pageviews");
+  //
+  // await collection
+  //   .updateMany(
+  //     { site: "topher@stupendousweb.com" },
+  //     { $set: { site: "642cf18729b904f37d859011" } }
+  //   )
+  //   .then(() => response.send("Good things come to those who wait."))
+  //   .finally(() => client.close());
+  //
+  //   await collection
+  //   .updateMany({}, { $rename: { created_at: "createdAt" } })
+  //   .then(() => response.send("Good things come to those who wait."))
+  //   .finally(() => client.close());
 }
