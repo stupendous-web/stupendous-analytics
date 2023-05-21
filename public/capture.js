8111bc662be0bae6eae73e4e54552be705b97929
stupendous-web <topher@stupendousweb.com>
2023-05-21 16:34:52 -0700

    Think twice, code once.

--- public/capture.js
+++ public/capture.js
@@ -1,1 +1,48 @@
-console.log(window.location.host);
+// const react = require("react");
+// const extractDomain = require("extract-domain");
+// const { getCookie, hasCookie, setCookie } = require("cookies-next");
+// const { v4: uuidv4 } = require("uuid");
+// const axios = require("axios");
+//
+// module.exports = function StupendousAnalytics({ site }) {
+//     react.useEffect(() => {
+//         const hostname = document.location.hostname;
+//         const path = document.location.pathname;
+//         const host = extractDomain(document.referrer);
+//         const referrer = document.referrer;
+//         const height = window.innerHeight;
+//         const width = window.innerWidth;
+//         const localTimestamp = new Date();
+//         !hasCookie("stupendous_analytics") &&
+//         setCookie("stupendous_analytics", uuidv4());
+//         const session = getCookie("stupendous_analytics");
+//
+//         const data = {
+//             site: site,
+//             hostname: hostname,
+//             path: path,
+//             host: host || "Direct",
+//             referrer: referrer,
+//             height: height,
+//             width: width,
+//             localTimestamp: localTimestamp,
+//             session: session,
+//         };
+//
+//         axios
+//             .post("https://stupendousanalytics.com/api/pageviews", data)
+//             .catch((error) => console.log("Stupendous Analytics Error:", error));
+//     });
+//
+//     return null;
+// };
+
+import axios from "axios";
+
+const data = {
+  site: new URL(window.location.href).searchParams.get("site"),
+};
+
+axios
+  .post("/api/pageviews", data)
+  .catch((error) => console.log("Stupendous Analytics Error: ", error));
